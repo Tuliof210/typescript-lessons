35182bf3f77ea57bfeb8d7bf78dfc777ac7d4347
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-05-22 11:10:14 -0300

    validate number in js

--- src/js-only.js
+++ src/js-only.js
@@ -3,7 +3,8 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  return num1 + num2;
+  // +value => turn value to number, or try 
+  return +num1 + +num2;
 }
 
 button.addEventListener("click", function() {

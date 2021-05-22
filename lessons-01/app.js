e2a7ad4d99d5e8b4dc28f314686e72efcfd1757c
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-05-22 15:53:41 -0300

    lesson 1 - core types

--- lessons-01/app.js
+++ lessons-01/app.js
@@ -1,1 +1,5 @@
-console.log('Your code goes here...');
+var add = function (n1, n2) { return n1 + n2; };
+var num1 = 5;
+var num2 = 2.8;
+var result = add(num1, num2);
+console.log(result);
--- lessons-01/app.ts
+++ lessons-01/app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...');
+const add = (n1: number, n2: number): number => n1 + n2;
+
+const num1: number = 5;
+const num2: number = 2.8;
+const result: number = add(num1, num2);
+
+console.log(result);
+
+/**
+ * The core primitive types in TypeScript are all lowercase!
+ */

dc505bdf901023f8fd00afc8139675756b43aa80
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-05-22 16:38:15 -0300

    tsconfig with tsc --init

--- lessons-01/app.js
+++ lessons-01/app.js
@@ -1,5 +1,17 @@
-var add = function (n1, n2) { return n1 + n2; };
+var add = function (n1, n2, show) {
+    if (show)
+        console.log(n1 + n2);
+    else
+        return n1 + n2;
+};
 var num1 = 5;
 var num2 = 2.8;
-var result = add(num1, num2);
-console.log(result);
+var result = add(num1, num2, true);
+/**
+ * The core primitive types in TypeScript are all lowercase!
+ *
+ * core types
+ * >>> number = integers and floats
+ * >>> string = text values
+ * >>> boolean = true|false
+ */
--- lessons-01/app.ts
+++ lessons-01/app.ts
@@ -1,11 +1,18 @@
-const add = (n1: number, n2: number): number => n1 + n2;
+const add = (n1: number, n2: number, show: boolean): number | void => {
+  const result = n1 + n2;
+  if (show) console.log(result);
+  else return result;
+};
 
 const num1: number = 5;
 const num2: number = 2.8;
-const result: number = add(num1, num2);
-
-console.log(result);
+const result: number = add(num1, num2, true);
 
 /**
  * The core primitive types in TypeScript are all lowercase!
+ *
+ * core types
+ * >>> number = integers and floats
+ * >>> string = text values
+ * >>> boolean = true|false
  */
